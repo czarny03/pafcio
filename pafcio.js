717da214f3108a60ad5277aacc8578e09434c0d3
czarny03 <sebastian.petryna@gmail.com>
2020-01-02 22:25:45 +0100

    Update pafcio.js

--- pafcio.js
+++ pafcio.js
@@ -1,1 +1,9 @@
 
+$(document).ready(function(){
+  $("#tak_btn_wrapper").hover(function(){
+    $("#tak_btn").css("position", "relative");
+    $("#tak_btn").css("left", "-200px;");
+    }, function(){
+    $("#tak_btn").css("position", "inherit");
+  });
+});
